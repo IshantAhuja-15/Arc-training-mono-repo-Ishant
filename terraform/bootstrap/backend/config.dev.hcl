602a85e16a70df89dc1cafac567282f780040622
IshantAhuja-15 <ishant.ahuja@sourcefuse.com>
2025-02-12 19:12:48 +0530

    Added Terraform modules for bootstrap and network

---
+++ terraform/bootstrap/backend/config.dev.hcl
@@ -0,0 +1,5 @@
+bucket         = "ishant-poc-terraform"
+key            = "bootstrap/terraform.tfstate"
+region         = "ap-south-1"
+dynamodb_table = "ishant-poc-terraform"
+encrypt        = true
---
+++ terraform/network/main.tf
@@ -0,0 +1,34 @@
+################################################################
+## network
+################################################################
+module "network" {
+  source = "https://github.com/sourcefuse/terraform-aws-arc-network"
+
+  namespace   = var.namespace
+  environment = var.environment
+
+  name                    = "arc-poc"
+  create_internet_gateway = true
+  # Enable vpc_flow_logs:If `s3_bucket_arn` is null, CloudWatch logging is enabled by default. If provided, S3 logging is enabled
+  vpc_flow_log_config = {
+    enable            = true
+    retention_in_days = 7
+    s3_bucket_arn     = ""
+  }
+
+
+  availability_zones = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  cidr_block         = "10.0.0.0/16"
+  vpc_endpoint_data = [
+    {
+      service            = "s3"
+      route_table_filter = "private"
+    },
+    {
+      service            = "dynamodb"
+      route_table_filter = "private"
+    }
+  ]
+
+  tags = module.tags.tags
+}
